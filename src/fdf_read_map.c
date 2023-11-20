5b568eada4ca16b37e6ad2b047b2bd755b5e8d0d
Marian Strba <marinstrba@gmail.com>
2023-11-20 12:32:45 +0100

    FIX: I fixed the issue where I was overriding data and also took care of all the memory leaks.

--- src/fdf_read_map.c
+++ src/fdf_read_map.c
@@ -6,38 +6,40 @@
 /*   By: mstrba <mstrba@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 17:50:23 by mstrba            #+#    #+#             */
-/*   Updated: 2023/11/19 20:33:09 by mstrba           ###   ########.fr       */
+/*   Updated: 2023/11/20 12:31:35 by mstrba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/fdf.h"
-#include "../lib/GNL/get_next_line.h"
 #include "../lib/LIBFT/libft.h"
 
-void	convert_to_cordinates(int	***cordinates, char	*line, int y_cordinate)
+int	convert_to_cordinates(int	***cordinates, char	*line, int y_cordinate)
 {
-	char	**seperated_characters;
-	int		index;
-	int		num;
+	char			**seperated_characters;
+	int				elements_added;
+	int				index;
+	int				num;
 
-	index = 0;
+	index = y_cordinate;
+	elements_added = 0;
 	seperated_characters = ft_split(line, ' ');
-	while (seperated_characters && seperated_characters[index])
+	while (seperated_characters && seperated_characters[elements_added])
 	{
 		(*cordinates)[index] = (int *) malloc (sizeof(int) * 3);
-		if (!(*cordinates)[index])
+		if (!(*cordinates)[index] || !seperated_characters)
 		{
 			fdf_failed_malloc();
-			exit(EXIT_FAILURE);
 		}
-		num = ft_atoi(seperated_characters[index]);
+		num = ft_atoi(seperated_characters[elements_added]);
 		(*cordinates)[index][2] = num;
 		(*cordinates)[index][1] = y_cordinate;
-		(*cordinates)[index][0] = index;
-		free(seperated_characters[index]);
+		(*cordinates)[index][0] = elements_added;
+		free(seperated_characters[elements_added]);
 		index++;
+		elements_added++;
 	}
 	free (seperated_characters);
+	return (elements_added);
 }
 
 void	fdf_reallocate_cordinates(int	***cordinates, int *bytes_allocated)
@@ -60,8 +62,8 @@ void	fdf_reallocate_cordinates(int	***cordinates, int *bytes_allocated)
 		fdf_failed_malloc();
 		exit(EXIT_FAILURE);
 	}
-	index = 0;
-	while (index < *bytes_allocated)
+	index = -1;
+	while (++index < *bytes_allocated)
 		new_cordinates[index] = (*cordinates)[index];
 	free(*cordinates);
 	*cordinates = new_cordinates;
@@ -73,22 +75,25 @@ int	**fdf_read_map(int fd)
 	char	*line;
 	int		bytes_allocated;
 	int		bytes_usage;
+	int		elements_to_add;
 	int		**cordinates;
 
 	bytes_usage = 0;
-	bytes_allocated = 1;
+	elements_to_add = 0;
+	bytes_allocated = 10;
 	cordinates = (int **)malloc(sizeof(int *) * bytes_allocated);
 	if (!cordinates)
 		return (NULL);
 	while (1)
 	{
-		if (bytes_usage + 1 == bytes_allocated)
-			fdf_reallocate_cordinates(&cordinates, &bytes_allocated);
 		line = get_next_line(fd);
 		if (!line)
 			break ;
-		convert_to_cordinates(&cordinates, line, bytes_usage);
-		bytes_usage++;
+		elements_to_add = convert_to_cordinates(&cordinates, line, bytes_usage);
+		free(line);
+		while (bytes_usage + elements_to_add > bytes_allocated)
+			fdf_reallocate_cordinates(&cordinates, &bytes_allocated);
+		bytes_usage += elements_to_add;
 	}
 	return (cordinates);
 }
